6e390dc1c2e75dcc33aa35cd682d65e04cd0bc83
Bozhidar Ivanov <145542302+Bozho94@users.noreply.github.com>
2025-04-11 12:35:40 +0300

    type of key of user changed to GUID

--- CinemaApp.Data/CinemaAppDbContext.cs
+++ CinemaApp.Data/CinemaAppDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CinemaApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CinemaApp.Data
 {
-    public class CinemaAppDbContext : IdentityDbContext
+    public class CinemaAppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public CinemaAppDbContext(DbContextOptions<CinemaAppDbContext> options)
             : base(options)
