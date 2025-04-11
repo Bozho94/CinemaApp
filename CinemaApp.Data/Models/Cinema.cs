85bbb02e19821aa71f916ed5d79e4538febf58dd
Bozhidar Ivanov <145542302+Bozho94@users.noreply.github.com>
2025-04-11 15:02:28 +0300

    Entity models added

---
+++ CinemaApp.Data/Models/Cinema.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client.Kerberos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Data.Models
+{
+    [Comment("Cinemas in the system")]
+    public class Cinema
+    {
+        [Key]
+        [Comment("Cinema identifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(256)]
+        [Comment("Cinema name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(256)]
+        [Comment("Cinema location")]
+        public string Location { get; set; } = null!;
+
+        [Comment("Shows if cinema is deleted")]
+        public bool IsDeleted { get; set; }
+
+        public List<CinemaMovie> CinemaMoives { get; set; } =
+            new List<CinemaMovie>();
+
+        public List<Ticket> Tickets { get; set; }
+            = new List<Ticket>();
+
+
+    }
+}
---
+++ CinemaApp.Data/Models/CinemaMovie.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaApp.Data.Models
+{
+    public class CinemaMovie
+    {
+        [Key]
+        public Guid MovieId { get; set; }
+
+        public Movie Movie { get; set; } = null!;
+
+        public Guid CinemaId { get; set; }
+
+        public Cinema Cinema { get; set; } = null!;
+
+        public int AvailableTickets { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        [Unicode(false)]
+        [MaxLength(5)]
+        public string Showtimes { get; set; } = "00000";
+
+
+
+
+
+
+    }
+}
